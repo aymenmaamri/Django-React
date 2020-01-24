d4f01d006e0f35deef5b00cb3d5f9b519dc356e7
aymenmaamri <maamriaymen1996@gmail.com>
2020-01-24 21:08:13 +0100

    Authentication added to Frontend

--- webapp/frontend/src/components/App.js
+++ webapp/frontend/src/components/App.js
@@ -1,12 +1,22 @@
 import React, { Component, Fragment } from "react";
 import ReactDom from "react-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../store";
-import { Header } from "./layout/header";
+import Header from "./layout/header";
 import Dashboard from "./leads/Dashboard";
+import Login from "./accounts/login";
+import Register from "./accounts/register";
 import Alerts from "./layout/Alerts";
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import PrivateRoute from "./common/PrivateRoute";
+import { loadUser } from "./../actions/auth";
 
 //Alert Options
 const alertOptions = {
@@ -15,17 +25,26 @@ const alertOptions = {
 };
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
   render() {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header />
-            <Alerts />
-            <div className="container">
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alerts />
+              <div className="container">
+                <Switch>
+                  <PrivateRoute exact path="/" component={Dashboard} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
