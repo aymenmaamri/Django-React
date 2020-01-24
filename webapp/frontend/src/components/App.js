67a1d21e92dae800c6e9a5662d9a6862ab45e6f5
aymenmaamri <maamriaymen1996@gmail.com>
2020-01-24 12:37:54 +0100

    initial commit

---
+++ webapp/frontend/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import store from "../store";
+import { Header } from "./layout/header";
+import Dashboard from "./leads/Dashboard";
+import Alerts from "./layout/Alerts";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+//Alert Options
+const alertOptions = {
+  timeout: 3000,
+  position: "top center"
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
+      </Provider>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.getElementById("app"));
